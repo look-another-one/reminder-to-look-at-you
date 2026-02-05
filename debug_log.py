b2e6258d20f068c45026d5178985f8c91edf768e
Grinding <ifightmyself@proton.me>
2026-02-05 13:40:37 +0500

    added diplaymanager, notification manager check in debuglog

--- debug_log.py
+++ debug_log.py
@@ -21,7 +21,16 @@ try:
 except: 
     print("subprocces failed!")
 
+# ---------- Notification Manager --------
+print("Notification Manager")
+try:
+    subprocess.run(['pgrep','-lfa','"dunst|xfce4-notifyd|gnome-shell|kdeinit|notification-daemon|mutter|swaync|mako"'])
+except:
+    print("notification manager check failed..")
 
-
-
-
+# --------- Display Manager ------- 
+print("Display Manager")
+try: 
+    subprocess.run(["echo","$XDG_CURRENT_DESKTOP"])
+except:
+    print("Display Manager check failed... ")
