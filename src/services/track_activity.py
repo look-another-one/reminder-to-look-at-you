117675cfee1f7cb33ef1805fcd002ca484dc4cd3
Humen being <ifightmyself@proton.me>
2026-06-24 13:25:57 +0500

    created class and track all activity

--- src/services/track_activity.py
+++ src/services/track_activity.py
@@ -2,30 +2,69 @@ import socket
 import os 
 import json
 
-def track_window():
-    '''
-    Connect to NIRI_SOCKET and get live EventStream Ouput
-    '''
-    sock = os.environ["NIRI_SOCKET"]
-    with socket.socket(socket.AF_UNIX,socket.SOCK_STREAM) as s:
-        s.connect(sock)
-        request = (json.dumps("EventStream") + "\n").encode()
-        s.sendall(request)
-        file = s.makefile("r")
-        for line in file:
-            output = json.loads(line.strip())
-            if "WindowFocusChanged" in output.keys():
-                print(f"Focused Changed to {output["WindowFocusChanged"]["id"]}")
-            elif "WindowOpenedOrChanged" in output.keys():
-                print(f"Window Opened, Title: {output["WindowOpenedOrChanged"]["window"]["title"]}")
-                print(f"Window Opened, ID: {output["WindowOpenedOrChanged"]["window"]["id"]}")
-                print(f"Window Opened, APP_ID: {output["WindowOpenedOrChanged"]["window"]["app_id"]}")
-            elif "WindowClosed" in output.keys():
-                print(f"WIndow Closed ID: {output["WindowClosed"]["id"]}")
-            else:
-                pass
+class TrackScreenTime:
+    def __init__(self):
+        self.registery = {}
+        self.focused_window = None
+        self.check_window_manager()
 
-if os.getenv("NIRI_SOCKET"):
-    track_window()
-else:
-    print("Are You Running Inside Niri?")
+    def check_window_manager(self):
+        if not os.getenv("NIRI_SOCKET"):
+            raise ValueError("niri socket not found")
+    
+    def connect_to_window_manager(self):
+        sock_path = os.environ["NIRI_SOCKET"]
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(sock_path)
+            request = (json.dumps("EventStream") + "\n").encode()
+            s.sendall(request)
+            
+            file = s.makefile("r")
+            for line in file:
+                output = json.loads(line.strip())
+                
+                if "WindowFocusChanged" in output:
+                    self.window_focus_changed(output["WindowFocusChanged"])
+                elif "WindowOpenedOrChanged" in output:
+                    self.window_opened(output["WindowOpenedOrChanged"])
+                elif "WindowClosed" in output:
+                    self.window_closed(output["WindowClosed"])
+
+    def window_opened(self, event_data):
+        window_info = event_data.get("window", {})
+        app_id = window_info.get("app_id")
+        window_id = window_info.get("id")
+        
+        if not app_id or not window_id:
+            return
+
+        if app_id not in self.registery:
+            self.registery[app_id] = [window_id]
+        elif window_id not in self.registery[app_id]:
+            self.registery[app_id].append(window_id)
+    
+    def window_focus_changed(self, event_data):
+        window_id = event_data.get("id")
+        self.focused_window = window_id
+
+    def window_closed(self, event_data):
+        window_id = event_data.get("id")
+        if not window_id:
+            return
+
+        for app_id, id_list in list(self.registery.items()):
+            if window_id in id_list:
+                id_list.remove(window_id)
+                
+                if not id_list:
+                    del self.registery[app_id]
+                
+                break
+        
+        if self.focused_window == window_id:
+            self.focused_window = None
+            
+
+if __name__ == "__main__":
+    start = TrackScreenTime()
+    start.connect_to_window_manager()
